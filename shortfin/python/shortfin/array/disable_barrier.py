eb8824aa364d80525e99e3cd4f7f1b193994b1cd
Stephen Baione <stbaione@amd.com>
2025-01-13 21:37:48 +0000

    Remove `disable_barrier.py`, Improve comments for `read_barrier`, `write_barrier`, and `disable_barrier`

--- shortfin/python/shortfin/array/disable_barrier.py
+++
@@ -1,6 +0,0 @@
-class DisableBarrier:
-    def __init__(self, delegate):
-        self.delegate = delegate
-
-    def __sfinv_marshal__(self, inv_capsule, ignored_resource_barrier):
-        self.delegate.__sfinv_marshal__(inv_capsule, 3) # 3 == ProgramResourceBarrier::NONE
