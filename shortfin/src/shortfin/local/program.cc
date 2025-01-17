f694d52ee988810d1928669e09d9498b4cb76c1a
Stephen Baione <stbaione@amd.com>
2025-01-17 15:37:55 +0000

    Default `mmap` to `false`, Add private `LoadMmap` function that uses legacy loading method to still allow mmaping param files

--- shortfin/src/shortfin/local/program.cc
+++ shortfin/src/shortfin/local/program.cc
@@ -633,6 +633,11 @@ void StaticProgramParameters::Load(std::filesystem::path file_path,
     options.format = file_path.extension().string();
   }
 
+  if (options.mmap) {
+    this->LoadMmap(file_path, options);
+    return;
+  }
+
   auto file_path_string = file_path.string();
   const iree_string_view_t path =
       iree_make_cstring_view(file_path_string.c_str());
@@ -644,6 +649,38 @@ void StaticProgramParameters::Load(std::filesystem::path file_path,
   SHORTFIN_THROW_IF_ERROR(
       iree_io_parse_file_index(to_iree_string_view(options.format), file_handle,
                                index_.get(), host_allocator_));
+}
+
+void StaticProgramParameters::LoadMmap(std::filesystem::path file_path,
+                                       LoadOptions options) {
+  SHORTFIN_TRACE_SCOPE_NAMED("StaticProgramParameters::LoadMmap");
+
+  iree_file_read_flags_t read_flags = IREE_FILE_READ_FLAG_MMAP;
+  iree_file_contents_t *file_contents = nullptr;
+  SHORTFIN_THROW_IF_ERROR(iree_file_read_contents(
+      file_path.string().c_str(), read_flags, host_allocator_, &file_contents));
+  iree_io_file_handle_release_callback_t release_callback = {
+      +[](void *user_data, iree_io_file_handle_primitive_t handle_primitive) {
+        iree_file_contents_t *file_contents = (iree_file_contents_t *)user_data;
+        iree_file_contents_free(file_contents);
+      },
+      file_contents,
+  };
+
+  // Wrap contents.
+  iree::io_file_handle_ptr file_handle;
+  iree_status_t status = iree_io_file_handle_wrap_host_allocation(
+      IREE_IO_FILE_ACCESS_READ, file_contents->buffer, release_callback,
+      host_allocator_, file_handle.for_output());
+  if (!iree_status_is_ok(status)) {
+    iree_file_contents_free(file_contents);
+    SHORTFIN_THROW_IF_ERROR(status);
+  }
+
+  // Parse.
+  SHORTFIN_THROW_IF_ERROR(iree_io_parse_file_index(
+      to_iree_string_view(options.format), file_handle.get(), index_.get(),
+      host_allocator_));
 }
 
 // -------------------------------------------------------------------------- //
