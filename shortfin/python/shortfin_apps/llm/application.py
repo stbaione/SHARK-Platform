b8dfbc83dc845eb22c900df1f35cfa0453690732
Stephen Baione <stbaione@amd.com>
2025-02-13 02:37:45 +0000

    Add license headers

--- shortfin/python/shortfin_apps/llm/application.py
+++ shortfin/python/shortfin_apps/llm/application.py
@@ -1,11 +1,13 @@
-import logging
+# Copyright 2024 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from fastapi import FastAPI
 
 from .lifecycle_hooks import lifespan
 from .routes import application_router, generation_router
-
-logger = logging.getLogger(__name__)
 
 
 def add_routes(app: FastAPI):
