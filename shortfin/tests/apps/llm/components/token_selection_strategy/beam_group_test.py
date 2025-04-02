d88a630cc2502f12741d9717ec4bbaaa38acb3ca
Stephen Baione <stbaione@amd.com>
2025-04-02 19:35:05 +0000

    Fix `beam_group` unit tests

--- shortfin/tests/apps/llm/components/token_selection_strategy/beam_group_test.py
+++ shortfin/tests/apps/llm/components/token_selection_strategy/beam_group_test.py
@@ -11,7 +11,7 @@ from shortfin_apps.llm.components.kvcache.base_attention_cache import (
 from shortfin_apps.llm.components.messages import LlmInferenceExecRequest
 from shortfin_apps.llm.components.token_selection_strategy.beam_group import (
     BeamGroup,
-    ExecRequestSelection,
+    Beam,
 )
 
 
@@ -29,16 +29,34 @@ def exec_req_list(exec_req, cache, dummy_pages):
     yield exec_reqs
 
 
+class DummyBeam(Beam):
+    def sample_logits(self):
+        pass
+
+    def update_score(self, value: float):
+        pass
+
+    def update_exec_req(self):
+        pass
+
+    def normalize_score(self, value: float):
+        pass
+
+    def final_score(self, value: float):
+        pass
+
+
 @pytest.mark.asyncio
 async def test_wait(exec_req_list):
     async def set_done(exec_reqs: List[LlmInferenceExecRequest]):
         for req in exec_reqs:
             req.done.set_success()
 
+    beams = [DummyBeam(exec_req) for exec_req in exec_req_list]
     beam_groups = BeamGroup(
         eos_token_id=-1,
         num_beams=len(exec_req_list),
-        exec_reqs=exec_req_list,
+        beams=beams,
         selection_callback=lambda x: None,
     )
     await asyncio.gather(*[beam_groups.wait(), set_done(exec_req_list)])
@@ -46,214 +64,147 @@ async def test_wait(exec_req_list):
         assert req.done._event.is_set()
 
 
-def test__update_exec_req(exec_req):
-    expected_start_position = exec_req.start_position + 1
-
-    # Score & Normalization is None
-    selection = ExecRequestSelection(exec_req, 0)
-    beam_group = BeamGroup(
-        eos_token_id=-1,
-        num_beams=1,
-        exec_reqs=[exec_req],
-        selection_callback=lambda _: None,
-    )
-    beam_group._update_exec_req(selection)
-    assert exec_req.input_token_ids[-1] == 0
-    assert exec_req.start_position == expected_start_position
-
-    expected_start_position += 1
-
-    # Score is not None
-    selection.score = 42.0
-    selection.token = 1
-    beam_group._update_exec_req(selection)
-    assert exec_req.input_token_ids[-1] == 1
-    assert exec_req.start_position == expected_start_position
-    assert exec_req.score == 42.0
-
-    expected_start_position += 1
-
-    # Normalization is not None
-    selection.score = None
-    selection.token = 2
-
-    def _normalization_function(req: LlmInferenceExecRequest):
-        req.accumulated_normalization += 1
-        return req
-
-    selection.normalization_function = _normalization_function
-    beam_group._update_exec_req(selection)
-    assert exec_req.input_token_ids[-1] == 2
-    assert exec_req.start_position == expected_start_position
-    assert exec_req.score == 42.0
-    assert exec_req.accumulated_normalization == 1
-
-    expected_start_position += 1
-
-    # Score & Normalization are not None
-    selection.token = 3
-    selection.score = 20.0
-    beam_group._update_exec_req(selection)
-    assert exec_req.input_token_ids[-1] == 3
-    assert exec_req.start_position == expected_start_position
-    assert exec_req.score == 20.0
-    assert exec_req.accumulated_normalization == 2
-
-
 def test_process_beams_one_req(exec_req):
-    def selection_callback(
-        active_reqs: LlmInferenceExecRequest, _: LlmInferenceExecRequest
-    ):
+    def selection_callback(active_beams: List[DummyBeam], _: List[DummyBeam]):
         selections = []
-        for req in active_reqs:
+        for beam in active_beams:
             token = 0
-            selections.append(ExecRequestSelection(req, token))
+            beam.last_token = token
+            selections.append(beam)
 
         return selections
 
+    beams = [DummyBeam(exec_req)]
     beam_groups = BeamGroup(
         eos_token_id=-1,
         num_beams=1,
-        exec_reqs=[exec_req],
+        beams=beams,
         selection_callback=selection_callback,
     )
 
     # Active
     beam_groups.process_beams()
-    assert beam_groups.active_exec_reqs == [exec_req]
-    assert len(beam_groups.completed_reqs) == 0
+    assert beam_groups.active_beams == beams
+    assert len(beam_groups.completed_beams) == 0
 
     # Completed
     beam_groups.eos_token_id = 0
     with patch.object(LlmInferenceExecRequest, "free_cache_pages") as free_cache_mock:
         beam_groups.process_beams()
-        assert len(beam_groups.active_exec_reqs) == 0
-        assert beam_groups.completed_reqs == {exec_req}
+        assert len(beam_groups.active_beams) == 0
+        assert beam_groups.completed_beams == beams
         free_cache_mock.assert_called_once()
 
 
 def test_process_beams_multiple_reqs(exec_req_list):
-    def selection_callback_no_completed(exec_reqs, _):
+    def selection_callback_no_completed(active_beams, _):
         selections = []
-        for req in exec_reqs:
+        for beam in active_beams:
             token = 0
+            beam.last_token = token
+            selections.append(beam)
+        return selections
+
+    def selection_callback_one_completed(active_beams, _):
+        active_beams[0].last_token = 1
+        selections = [active_beams[0]]
+        for beam in active_beams[1:]:
+            beam.last_token = 0
             selections.append(
-                ExecRequestSelection(
-                    req,
-                    token,
-                )
+                beam,
             )
         return selections
 
-    def selection_callback_one_completed(exec_reqs, _):
-        selections = [
-            ExecRequestSelection(
-                exec_reqs[0],
-                1,
-            )
-        ]
-        for req in exec_reqs[1:]:
+    def selection_callback_all_completed(active_beams, _):
+        selections = []
+        for beam in active_beams:
+            beam.last_token = 1
             selections.append(
-                ExecRequestSelection(
-                    req,
-                    token=0,
-                )
-            )
-        return selections
-
-    def selection_callback_all_completed(exec_reqs, _):
-        selections = [
-            ExecRequestSelection(
-                exec_reqs[0],
-                1,
-            )
-        ]
-        for req in exec_reqs[1:]:
-            selections.append(
-                ExecRequestSelection(
-                    req,
-                    token=1,
-                )
+                beam,
             )
         return selections
 
     req_list = exec_req_list.copy()
+    beams = [DummyBeam(req) for req in req_list]
     beam_group = BeamGroup(
         eos_token_id=1,
         num_beams=len(req_list),
-        exec_reqs=req_list,
+        beams=beams,
         selection_callback=selection_callback_no_completed,
     )
     beam_group.process_beams()
-    assert set(beam_group.active_exec_reqs) == set(req_list)
-    assert len(beam_group.completed_reqs) == 0
+    assert beam_group.active_beams == beams
+    assert len(beam_group.completed_beams) == 0
 
     req_list = exec_req_list.copy()
+    beams = [DummyBeam(req) for req in req_list]
     beam_group = BeamGroup(
         eos_token_id=1,
         num_beams=len(req_list),
-        exec_reqs=req_list,
+        beams=beams,
         selection_callback=selection_callback_one_completed,
     )
-    expected = {beam_group.active_exec_reqs[0]}
-    active = set(beam_group.active_exec_reqs[1:])
+    expected = [beam_group.active_beams[0]]
+    active = beam_group.active_beams[1:]
     with patch.object(LlmInferenceExecRequest, "free_cache_pages") as free_cache_mock:
         beam_group.selection_callback = selection_callback_one_completed
         beam_group.process_beams()
-        assert set(beam_group.active_exec_reqs) == active
-        assert beam_group.completed_reqs == expected
+        assert beam_group.active_beams == active
+        assert beam_group.completed_beams == expected
         free_cache_mock.assert_called_once()
 
         # Complete another req
-        expected.add(beam_group.active_exec_reqs[0])
-        active.remove(beam_group.active_exec_reqs[0])
+        expected.append(beam_group.active_beams[0])
+        active.remove(beam_group.active_beams[0])
         beam_group.process_beams()
-        assert set(beam_group.active_exec_reqs) == active
-        assert beam_group.completed_reqs == expected
+        assert beam_group.active_beams == active
+        assert beam_group.completed_beams == expected
         assert free_cache_mock.call_count == 2
 
     req_list = exec_req_list.copy()
+    beams = [DummyBeam(req) for req in req_list]
     beam_group = BeamGroup(
         eos_token_id=1,
         num_beams=len(req_list),
-        exec_reqs=req_list,
+        beams=beams,
         selection_callback=selection_callback_all_completed,
     )
     # All completed
     with patch.object(LlmInferenceExecRequest, "free_cache_pages") as free_cache_mock:
         beam_group.process_beams()
-        assert len(beam_group.active_exec_reqs) == 0
-        assert beam_group.completed_reqs == set(req_list)
-        assert free_cache_mock.call_count == len(req_list)
+        assert len(beam_group.active_beams) == 0
+        assert beam_group.completed_beams == beams
+        assert free_cache_mock.call_count == len(beams)
 
 
 @pytest.mark.asyncio
 async def test_clean_up(exec_req_list):
+    beams = [DummyBeam(req) for req in exec_req_list]
     beam_group = BeamGroup(
         eos_token_id=-1,
         num_beams=len(exec_req_list),
-        exec_reqs=exec_req_list,
+        beams=beams,
         selection_callback=lambda x: None,
     )
     with patch.object(LlmInferenceExecRequest, "free_cache_pages") as free_cache_mock:
         # All active
         beam_group.clean_up()
-        assert free_cache_mock.call_count == len(beam_group.active_exec_reqs)
+        assert free_cache_mock.call_count == len(beam_group.active_beams)
 
         free_cache_mock.reset_mock()
 
         # All completed
-        beam_group.completed_reqs = set(exec_req_list)
-        beam_group.active_exec_reqs = []
+        beam_group.completed_beams = beams
+        beam_group.active_beams = []
         beam_group.clean_up()
-        assert free_cache_mock.call_count == len(beam_group.completed_reqs)
+        assert free_cache_mock.call_count == len(beam_group.completed_beams)
 
         free_cache_mock.reset_mock()
 
         # Mixture of both
-        beam_group.completed_reqs = set(exec_req_list[: len(exec_req_list) // 2])
-        beam_group.active_exec_reqs = exec_req_list[len(exec_req_list) // 2 :]
+        beam_group.completed_beams = beams[: len(exec_req_list) // 2]
+        beam_group.active_beams = beams[len(exec_req_list) // 2 :]
         beam_group.clean_up()
-        assert free_cache_mock.call_count == len(beam_group.completed_reqs) + len(
-            beam_group.active_exec_reqs
+        assert free_cache_mock.call_count == len(beam_group.completed_beams) + len(
+            beam_group.active_beams
         )
