9045819203d533e48a77188614a1042f2f48da1d
Stephen Baione <stbaione@amd.com>
2025-09-10 16:44:33 +0000

    Move repeated functions in `Scheduler` and `ChunkScheduler` to `AbstractScheduler`

--- shortfin/python/shortfin_apps/llm/components/scheduler.py
+++ shortfin/python/shortfin_apps/llm/components/scheduler.py
@@ -123,6 +123,18 @@ class WorkloadBuilder:
 
 
 class AbstractScheduler(ABC):
+    def __init__(self, *, ideal_batch_size: int) -> None:
+        self._ideal_batch_size = ideal_batch_size
+        self._unreserved_strobe = None
+        self._wid = 0
+        self._preferred_groups = 1
+
+        # Mapping from RID to the corresponding workgroup ID
+        self._workgroup_placement = {}
+
+        # Mapping from workgroup ID to the Workgroup tracker:
+        self._workgroups = {}
+
     @abstractmethod
     def schedule_job(self, task: LlmTaskInput):
         pass
@@ -142,25 +154,6 @@ class AbstractScheduler(ABC):
     @abstractmethod
     def handle_completed(self, rid: str) -> bool:
         pass
-
-
-class Scheduler(AbstractScheduler):
-    def __init__(self, *, ideal_batch_size):
-        self._ideal_batch_size = ideal_batch_size
-        self._unreserved_strobe = None
-        self._wid = 0
-        self._preferred_groups = 1
-
-        self.pending: List[LlmTaskInput] = []
-
-        # Mapping from RID to the corresponding workgroup ID
-        self._workgroup_placement = {}
-
-        # Mapping from workgroup ID to the Workgroup tracker:
-        self._workgroups = {}
-
-    def schedule_job(self, task: LlmTaskInput):
-        self.pending.append(task)
 
     def _group_jobs(
         self, rid_map: Dict[str, List[LlmTaskInput]], strobe
@@ -212,6 +205,79 @@ class Scheduler(AbstractScheduler):
                 workload_builder.add_work(unreserved)
 
         return workload_builder
+
+    def _schedule_reservation(self, *, rid, count):
+        if rid in self._workgroup_placement:
+            wid = self._workgroup_placement[rid]
+            workgroup = self._workgroups[wid]
+            existing = workgroup.member_count(rid=rid)
+            if workgroup.can_add(count - existing):
+                workgroup.resize(rid=rid, count=count)
+                return
+
+            # If we cannot fit the workgroup in the existing dispatch we need to redistribute:
+            workgroup.remove(rid=rid)
+            self._workgroup_placement.pop(rid)
+            if workgroup.is_empty():
+                self._workgroups.pop(wid)
+
+        def schedule_new():
+            self._wid = self._wid + 1
+            wid = self._wid
+
+            wg = Workgroup(wid=wid, max_size=self._ideal_batch_size)
+            wg.resize(rid=rid, count=count)
+            self._workgroups[wid] = wg
+            self._workgroup_placement[rid] = wid
+
+        # Guarantee there are two workgroups and schedule full count:
+        if len(self._workgroups) < self._preferred_groups:
+            schedule_new()
+            return
+
+        # Search for a workgroup with space
+        workgroup_sel = None
+        for wid in self._workgroups.keys():
+            workgroup = self._workgroups[wid]
+
+            if workgroup.can_add(count):
+                workgroup_sel = workgroup
+                break
+
+        # Schedule if no home found:
+        if workgroup_sel is None:
+            schedule_new()
+            return
+
+        workgroup_sel.resize(count=count, rid=rid)
+        self._workgroup_placement[rid] = workgroup_sel.wid
+
+    def _remove(self, *, rid):
+        if rid not in self._workgroup_placement:
+            return
+
+        wid = self._workgroup_placement[rid]
+        workgroup = self._workgroups[wid]
+
+        workgroup.remove(rid=rid)
+        if workgroup.is_empty():
+            self._workgroups.pop(wid)
+
+        for wid in self._workgroups:
+            workgroup = self._workgroups[wid]
+            if workgroup.has_member(rid=rid):
+                break
+
+        self._workgroup_placement.pop(rid)
+
+
+class Scheduler(AbstractScheduler):
+    def __init__(self, *, ideal_batch_size):
+        self.pending: List[LlmTaskInput] = []
+        super().__init__(ideal_batch_size=ideal_batch_size)
+
+    def schedule_job(self, task: LlmTaskInput):
+        self.pending.append(task)
 
     def should_execute(self, strobe) -> List[List[LlmTaskInput]]:
         pending = self.pending
@@ -236,70 +302,6 @@ class Scheduler(AbstractScheduler):
 
         return workload_builder.get_jobs()
 
-    def _schedule_reservation(self, *, rid, count):
-        if rid in self._workgroup_placement:
-            wid = self._workgroup_placement[rid]
-            workgroup = self._workgroups[wid]
-            existing = workgroup.member_count(rid=rid)
-            if workgroup.can_add(count - existing):
-                workgroup.resize(rid=rid, count=count)
-                return
-
-            # If we cannot fit the workgroup in the existing dispatch we need to redistribute:
-            workgroup.remove(rid=rid)
-            self._workgroup_placement.pop(rid)
-            if workgroup.is_empty():
-                self._workgroups.pop(wid)
-
-        def schedule_new():
-            self._wid = self._wid + 1
-            wid = self._wid
-
-            wg = Workgroup(wid=wid, max_size=self._ideal_batch_size)
-            wg.resize(rid=rid, count=count)
-            self._workgroups[wid] = wg
-            self._workgroup_placement[rid] = wid
-
-        # Guarantee there are two workgroups and schedule full count:
-        if len(self._workgroups) < self._preferred_groups:
-            schedule_new()
-            return
-
-        # Search for a workgroup with space
-        workgroup_sel = None
-        for wid in self._workgroups.keys():
-            workgroup = self._workgroups[wid]
-
-            if workgroup.can_add(count):
-                workgroup_sel = workgroup
-                break
-
-        # Schedule if no home found:
-        if workgroup_sel is None:
-            schedule_new()
-            return
-
-        workgroup_sel.resize(count=count, rid=rid)
-        self._workgroup_placement[rid] = workgroup_sel.wid
-
-    def _remove(self, *, rid):
-        if rid not in self._workgroup_placement:
-            return
-
-        wid = self._workgroup_placement[rid]
-        workgroup = self._workgroups[wid]
-
-        workgroup.remove(rid=rid)
-        if workgroup.is_empty():
-            self._workgroups.pop(wid)
-
-        for wid in self._workgroups:
-            workgroup = self._workgroups[wid]
-            if workgroup.has_member(rid=rid):
-                break
-
-        self._workgroup_placement.pop(rid)
-
     def handle_scheduler(self, msg) -> bool:
         if isinstance(msg, UpdateWorkload):
             if msg.count == 0:
@@ -320,19 +322,9 @@ class Scheduler(AbstractScheduler):
 
 class ChunkScheduler(AbstractScheduler):
     def __init__(self, *, ideal_batch_size):
-        self._ideal_batch_size = ideal_batch_size
-        self._unreserved_strobe = None
-        self._wid = 0
-        self._preferred_groups = 1
-
         self.pending: Dict[str, List[LlmTaskInput]] = {}
         self.ready: set[LlmTaskInput] = set()
-
-        # Mapping from RID to the corresponding workgroup ID
-        self._workgroup_placement = {}
-
-        # Mapping from workgroup ID to the Workgroup tracker:
-        self._workgroups = {}
+        super().__init__(ideal_batch_size=ideal_batch_size)
 
     def schedule_job(self, task: LlmTaskInput):
         if self.pending.get(task.rid) is None:
@@ -340,57 +332,6 @@ class ChunkScheduler(AbstractScheduler):
             self.pending[task.rid] = []
         else:
             self.pending[task.rid].append(task)
-
-    def _group_jobs(
-        self, rid_map: Dict[str, List[LlmTaskInput]], strobe
-    ) -> WorkloadBuilder:
-        workload_builder = WorkloadBuilder(ideal_batch_size=self._ideal_batch_size)
-
-        # Split out reserved and unreserved jobs:
-        reserved = {
-            rid: rid_map[rid] for rid in rid_map if rid in self._workgroup_placement
-        }
-        unreserved = list(
-            itertools.chain(
-                *[
-                    rid_map[rid]
-                    for rid in rid_map
-                    if rid not in self._workgroup_placement
-                ]
-            )
-        )
-
-        # Schedule all jobs known to the reservation system
-        for workgroup_id in self._workgroups.keys():
-            workgroup = self._workgroups[workgroup_id]
-            to_schedule = workgroup.schedule(pending=reserved, strobe=strobe)
-            if to_schedule is not None:
-                workload_builder.add_work(to_schedule)
-
-        # Slot any unreserved work into empty ideal space
-        if len(unreserved) > 0 and workload_builder.available() > 0:
-            available = workload_builder.available()
-            workload_builder.add_work(unreserved[:available])
-            unreserved = unreserved[available:]
-
-        # Dispatch ideal batch size if we accumulated enough:
-        while len(unreserved) >= self._ideal_batch_size:
-            new_job = unreserved[: self._ideal_batch_size]
-            unreserved = unreserved[self._ideal_batch_size :]
-            workload_builder.add_work(new_job)
-            self._unreserved_strobe = None
-
-        # If we have remaining unreserved jobs
-        if len(unreserved) > 0:
-            # Schedule the strobe for a future follow up:
-            if self._unreserved_strobe is None:
-                self._unreserved_strobe = strobe
-            # If we strobed previously we should add the remaining work:
-            elif strobe - self._unreserved_strobe > 1:
-                self._unreserved_strobe = None
-                workload_builder.add_work(unreserved)
-
-        return workload_builder
 
     def should_execute(self, strobe) -> List[List[LlmTaskInput]]:
         jobs = self.ready
@@ -412,70 +353,6 @@ class ChunkScheduler(AbstractScheduler):
         self.ready = self.ready | jobs
 
         return workload_builder.get_jobs()
-
-    def _schedule_reservation(self, *, rid, count):
-        if rid in self._workgroup_placement:
-            wid = self._workgroup_placement[rid]
-            workgroup = self._workgroups[wid]
-            existing = workgroup.member_count(rid=rid)
-            if workgroup.can_add(count - existing):
-                workgroup.resize(rid=rid, count=count)
-                return
-
-            # If we cannot fit the workgroup in the existing dispatch we need to redistribute:
-            workgroup.remove(rid=rid)
-            self._workgroup_placement.pop(rid)
-            if workgroup.is_empty():
-                self._workgroups.pop(wid)
-
-        def schedule_new():
-            self._wid = self._wid + 1
-            wid = self._wid
-
-            wg = Workgroup(wid=wid, max_size=self._ideal_batch_size)
-            wg.resize(rid=rid, count=count)
-            self._workgroups[wid] = wg
-            self._workgroup_placement[rid] = wid
-
-        # Guarantee there are two workgroups and schedule full count:
-        if len(self._workgroups) < self._preferred_groups:
-            schedule_new()
-            return
-
-        # Search for a workgroup with space
-        workgroup_sel = None
-        for wid in self._workgroups.keys():
-            workgroup = self._workgroups[wid]
-
-            if workgroup.can_add(count):
-                workgroup_sel = workgroup
-                break
-
-        # Schedule if no home found:
-        if workgroup_sel is None:
-            schedule_new()
-            return
-
-        workgroup_sel.resize(count=count, rid=rid)
-        self._workgroup_placement[rid] = workgroup_sel.wid
-
-    def _remove(self, *, rid):
-        if rid not in self._workgroup_placement:
-            return
-
-        wid = self._workgroup_placement[rid]
-        workgroup = self._workgroups[wid]
-
-        workgroup.remove(rid=rid)
-        if workgroup.is_empty():
-            self._workgroups.pop(wid)
-
-        for wid in self._workgroups:
-            workgroup = self._workgroups[wid]
-            if workgroup.has_member(rid=rid):
-                break
-
-        self._workgroup_placement.pop(rid)
 
     def handle_scheduler(self, msg) -> bool:
         if isinstance(msg, UpdateWorkload):
