f90b6dbbc77e5e9ccec23a5a0ecd679b91c94be9
pdhirajkumarprasad <160474250+pdhirajkumarprasad@users.noreply.github.com>
2025-09-26 09:30:30 +0530

    fix for auto-detection of dtype (#2343)
    fix for auto-detection of dtype
    
    Signed-off-by: pdhirajkumarprasad <dhirajp@amd.com>

--- scripts/export_and_compile.sh
+++ scripts/export_and_compile.sh
@@ -88,7 +88,14 @@ elif [[ $DTYPE = "fp8" ]]; then
         --output-mlir=$OUTPUT_DIR/output.mlir \
         --output-config=$OUTPUT_DIR/config_attn.json \
         --bs-prefill=$PREFILL_BS --bs-decode=$DECODE_BS --attention-kernel sharktank \
-        --use-hf --kv-cache-dtype=$KV_CACHE_DTYPE --device-block-count 8043
+        --use-hf  --device-block-count 8043
+
+elif [[ $DTYPE = "llama-70B-FP8" ]]; then
+    python3 -m sharktank.examples.export_paged_llm_v1 --irpa-file=$IRPA_PATH \
+        --output-mlir=$OUTPUT_DIR/output.mlir \
+        --output-config=$OUTPUT_DIR/config_attn.json \
+        --bs-prefill=$PREFILL_BS --bs-decode=$DECODE_BS --attention-kernel sharktank \
+        --use-hf --kv-cache-dtype=float8_e4m3fnuz --device-block-count 8043
 
 elif [[ $DTYPE = "mistral_fp8" ]]; then
     python3 -m sharktank.examples.export_paged_llm_v1 --irpa-file=$IRPA_PATH \
