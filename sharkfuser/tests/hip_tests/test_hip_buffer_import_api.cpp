68c79817510c5621720f72cf220c6d15d3be727d
Aaron St George <aaronstgeorge@gmail.com>
2025-10-10 18:31:16 -0600

    [Fusilli] Expand hip `Buffer::import` test (#2486)
    As titled.

--- sharkfuser/tests/hip_tests/test_hip_buffer_import_api.cpp
+++ sharkfuser/tests/hip_tests/test_hip_buffer_import_api.cpp
@@ -4,16 +4,21 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <fusilli.h>
+#include <utils.h>
+
+#include <catch2/catch_test_macros.hpp>
 #include <hip/hip_runtime.h>
 #include <iree/base/status.h>
 #include <iree/hal/api.h>
 #include <iree/runtime/api.h>
 
-#include <catch2/catch_test_macros.hpp>
 #include <cstdint>
 #include <cstdio>
 
 #include "hip_kernels.h"
+
+using namespace fusilli;
 
 // Utility macro to check status of HIP functions that are set with nodiscard.
 #define HIP_REQUIRE_SUCCESS(expr)                                              \
@@ -43,40 +48,49 @@ TEST_CASE("proof of life for HIP", "[hip_tests]") {
   launchHelloKernel(dim3(1), dim3(4));
 
   HIP_REQUIRE_SUCCESS(hipDeviceSynchronize());
+
+  HIP_REQUIRE_SUCCESS(hipFree(ptr));
+}
+
+static void testBufferReleaseCallback(void *didCleanupBuffer,
+                                      iree_hal_buffer_t *) {
+  *(bool *)didCleanupBuffer = true;
 }
 
 TEST_CASE("Buffer import", "[hip_tests]") {
   // --------------------------------------------------------------------------
-  //  Test for the externally managed buffers imported as IREE HAL Buffer View
-  //
-  //  `hipMalloc`'ed `void*` -> `iree_hal_buffer_view` (via `import_buffer`)
+  //  Test that externally managed buffers, imported as IREE HAL Buffer View,
+  //  and finally as a fusilli Buffer, are created successfully and cleaned up
+  //  correctly.
   // --------------------------------------------------------------------------
+
+  Handle handle = FUSILLI_REQUIRE_UNWRAP(Handle::create(Backend::GFX942));
+
+  // IREE allocators.
+  iree_hal_allocator_t *deviceAllocator = iree_hal_device_allocator(handle);
+  iree_allocator_t hostAllocator = iree_allocator_system();
 
   // Pointer to hipMalloc'ed buffer on device.
   void *ptr;
-  HIP_REQUIRE_SUCCESS(hipMalloc(&ptr, sizeof(float) * 64));
+  const size_t elementCount = 64;
+  const size_t bufferSize = elementCount * sizeof(float);
+  HIP_REQUIRE_SUCCESS(hipMalloc(&ptr, sizeof(float) * elementCount));
 
-  // Get runtime instance.
-  iree_runtime_instance_options_t instance_options;
-  iree_runtime_instance_options_initialize(&instance_options);
-  iree_runtime_instance_options_use_all_available_drivers(&instance_options);
-  iree_runtime_instance_t *instance = nullptr;
-  REQUIRE(iree_status_is_ok(iree_runtime_instance_create(
-      &instance_options, iree_allocator_system(), &instance)));
+  // Callback set on IREE buffer and called in `iree_hal_hip_buffer_destroy`.
+  bool didCleanupBuffer = false;
+  iree_hal_buffer_release_callback_t callback = {
+      .fn = testBufferReleaseCallback, .user_data = &didCleanupBuffer};
 
-  // Get runtime device.
-  iree_hal_device_t *device = nullptr;
-  REQUIRE(iree_status_is_ok(iree_runtime_instance_try_create_default_device(
-      instance, iree_make_cstring_view("hip"), &device)));
-
-  // Get device allocator.
-  iree_hal_allocator_t *allocator = iree_hal_device_allocator(device);
-
-  // Create external buffer wrapping the void*.
-  iree_hal_external_buffer_t external_buffer = {
+  // Import external buffer into IREE runtime.
+  iree_hal_buffer_params_t bufferParams = {
+      .usage = IREE_HAL_BUFFER_USAGE_DEFAULT,
+      .access = IREE_HAL_MEMORY_ACCESS_READ | IREE_HAL_MEMORY_ACCESS_WRITE,
+      .type = IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL,
+  };
+  iree_hal_external_buffer_t externalBuffer = {
       .type = IREE_HAL_EXTERNAL_BUFFER_TYPE_DEVICE_ALLOCATION,
       .flags = 0,
-      .size = sizeof(float) * 64,
+      .size = static_cast<iree_device_size_t>(bufferSize),
       .handle =
           {
               .device_allocation =
@@ -85,27 +99,44 @@ TEST_CASE("Buffer import", "[hip_tests]") {
                   },
           },
   };
-
-  // Set buffer parameters.
-  iree_hal_buffer_params_t params = {0};
-  // Where to allocate (host or device).
-  params.type = IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL;
-  // Access to allow to this memory.
-  params.access = IREE_HAL_MEMORY_ACCESS_ALL;
-  // Intended usage of the buffer (transfers, dispatches, etc).
-  params.usage = IREE_HAL_BUFFER_USAGE_DEFAULT;
-
-  // Create imported buffer to be populated with externally managed buffer.
-  iree_hal_buffer_t *out_buffer = nullptr;
-
-  iree_hal_buffer_release_callback_t release_callback =
-      iree_hal_buffer_release_callback_null();
-
-  // Import buffer.
+  iree_hal_buffer_t *importedBuffer = nullptr;
   REQUIRE(iree_status_is_ok(iree_hal_allocator_import_buffer(
-      allocator, params, &external_buffer, release_callback, &out_buffer)));
+      /*allocator=*/deviceAllocator, /*params=*/bufferParams,
+      /*external_buffer=*/&externalBuffer, /*release_callback=*/callback,
+      /*out_buffer=*/&importedBuffer)));
 
-  // Cleanup.
-  iree_hal_device_release(device);
-  iree_runtime_instance_release(instance);
+  // Create an IREE buffer view for external buffer.
+  const iree_hal_dim_t shape[] = {elementCount};
+  const size_t shapeRank = 1;
+  iree_hal_buffer_view_t *outBufferView = nullptr;
+  REQUIRE(iree_status_is_ok(iree_hal_buffer_view_create(
+      /*buffer=*/importedBuffer,
+      /*shape_rank=*/shapeRank,
+      /*shape=*/shape,
+      /*element_type=*/IREE_HAL_ELEMENT_TYPE_FLOAT_32,
+      /*encoding_type=*/IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
+      /*host_allocator=*/hostAllocator,
+      /*out_buffer_view=*/&outBufferView)));
+
+  // The buffer view holds a reference to buffer and will handle release.
+  iree_hal_buffer_release(importedBuffer);
+
+  {
+    // Buffer is a RAII type that retains the buffer view.
+    Buffer fusilliBufferResult =
+        FUSILLI_REQUIRE_UNWRAP(fusilli::Buffer::import(outBufferView));
+
+    // The IREE buffer view and IREE buffer will now be tied to fusilli Buffer's
+    // lifetime.
+    iree_hal_buffer_view_release(outBufferView);
+
+    // Ensure that IREE buffer has not been released.
+    REQUIRE_FALSE(didCleanupBuffer);
+  }
+
+  // Ensure that fusilli Buffer released the IREE buffer view which in turn
+  // should release the buffer.
+  REQUIRE(didCleanupBuffer);
+
+  HIP_REQUIRE_SUCCESS(hipFree(ptr));
 }
