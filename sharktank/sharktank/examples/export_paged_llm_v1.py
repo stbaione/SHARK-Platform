2141bb7b9ff8cd331e9dd5835ea2c95a42e0be2e
Sonbol Yazdanbakhsh <129094015+oyazdanb@users.noreply.github.com>
2025-10-14 11:48:07 -0400

    [sharktank] add sliding window in export (#2447)
    Optimize export for sliding window models

--- sharktank/sharktank/examples/export_paged_llm_v1.py
+++ sharktank/sharktank/examples/export_paged_llm_v1.py
@@ -52,7 +52,12 @@ def export_llm_v1(
     def generate_batch_prefill(bs: int):
         # torch.export.Dim would make min at least 2
         block_dim_min = 2
-        block_dim_max = ceildiv(hp.context_length, llama_config.block_seq_stride) - 1
+
+        effective_context = (
+            hp.sliding_window if hp.sliding_window else hp.context_length
+        )
+        block_dim_max = ceildiv(effective_context, llama_config.block_seq_stride) - 1
+
         seq_len_blocks_dim = torch.export.Dim(
             "seq_len_blocks_dim", min=block_dim_min, max=block_dim_max
         )
@@ -144,7 +149,12 @@ def export_llm_v1(
     def generate_batch_decode(bs: int):
         # torch.export.Dim would make min at least 2
         block_dim_min = 2
-        block_dim_max = ceildiv(hp.context_length, llama_config.block_seq_stride) - 1
+
+        effective_context = (
+            hp.sliding_window if hp.sliding_window else hp.context_length
+        )
+        block_dim_max = ceildiv(effective_context, llama_config.block_seq_stride) - 1
+
         seq_len_blocks_dim = torch.export.Dim(
             "seq_len_blocks_dim", min=block_dim_min, max=block_dim_max
         )
